c19f58a84958ff94ac623efbd5500eec6e11b94b
Mark Szymczyk <mark@meandmark.com>
2017-03-27 15:19:10 -0400

    Initial Commit

---
+++ TemperatureConverter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TemperatureConverter
+//
+//  Created by mark on 3/27/17.
+//  Copyright © 2017 Me and Mark Publishing. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
